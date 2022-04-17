d5e81bd391ad8f3fe3697c8419307d073ca43c61
Maxim <atm.tsvetk@gmail.com>
2022-04-17 20:25:36 +0500

    Init

---
+++ Client/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Client.Views;
+using Xamarin.Forms;
+
+namespace Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
